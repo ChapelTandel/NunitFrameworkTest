d1ef30c566e42167cf45010cfa26187651e4d685
chapel tandel <dev_damani@yahoo.com>
2020-07-25 18:01:52 +0100

    first commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NunitFrameworkTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NunitFrameworkTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b568af13-f0ac-45dc-af61-c042f88c5daa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NunitFrameworkTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void EmptyString()
+        {
+            const int expected = 0;
+            var actual = _calculator.Add(string.Empty);
+            Assert.That(actual == expected);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("15", 15)]
+        [TestCase("100", 100)]
+        public void OneNumber(string num, int result)
+        {
+            var actual = _calculator.Add(num);
+            var expected = result;
+            Assert.That(actual == expected);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("15,4", 19)]
+        [TestCase("100, 1000", 1100)]
+        public void TwoNumber(string num, int result)
+        {
+            var actual = _calculator.Add(num);
+            var expected = result;
+            Assert.That(actual == expected);
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(string num)
+        {
+            if (num == string.Empty)
+                return 0;
+
+            var intArr = ConvertStringToIntArray(num);
+
+            return intArr.Sum();
+        }
+
+        private static IEnumerable<int> ConvertStringToIntArray(string num)
+        {
+            return num.Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+        }
+    }
+}
